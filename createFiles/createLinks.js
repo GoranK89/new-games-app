1150caf71f89c1eba99da374d878b88f3b9231fc
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-05-16 22:39:21 +0200

    generating game links and symlinks functional

--- createFiles/createLinks.js
+++ createFiles/createLinks.js
@@ -1,40 +1,56 @@
 const fs = require("fs");
 
+const specialGameProviders = ["MGSD", "MGSM", "NETEM", "NETED"];
+
 const generateGameLink = (gameCode) => {
   const gameProvider = gameCode.split("_")[0];
-  const baseGameLink = `games[]=${gameProvider}/${gameCode}`;
+  const baseGameLink = `games[]=${gameProvider}/${gameCode}\n`;
 
-  if (
-    gameProvider === "MGSD" ||
-    gameProvider === "MGSM" ||
-    gameProvider === "NETEM" ||
-    gameProvider === "NETED"
-  ) {
+  if (specialGameProviders.includes(gameProvider)) {
     const exceptionCode = gameProvider.slice(0, -1);
-    const gameLink = `games[]=${exceptionCode}/${gameCode}`;
-    return gameLink;
+    return `games[]=${exceptionCode}/${gameCode}\n`;
   } else {
     return baseGameLink;
   }
 };
 
+const generateSymlink = (gameCode) => {
+  const gameProvider = gameCode.split("_")[0];
+  const gameLink = generateGameLink(gameCode);
+  const modifiedGameLink = gameLink.replace("games[]=", "").split("_");
+  modifiedGameLink.pop();
+  const bindSymlinkTo = modifiedGameLink.join("_");
+
+  const baseSymlink = `symlinks[]=${gameProvider}/${gameCode},${bindSymlinkTo}\n`;
+
+  if (specialGameProviders.includes(gameProvider)) {
+    const exceptionCode = gameProvider.slice(0, -1);
+    return `symlinks[]=${exceptionCode}/${gameCode},${bindSymlinkTo}\n`;
+  } else {
+    return baseSymlink;
+  }
+};
+
 const createLink = (path, gameCode) => {
   const gameLink = generateGameLink(gameCode);
-
-  // if (!fs.existsSync(`${path}/links.txt`)) return;
-
-  // fs.readFile(`${path}/links.txt`, "utf8", (err, data) => {
-  //   let existingLinks = data.trim();
-  //   existingLinks += "\n";
-  //   const existingEntries = existingLinks.split("\n");
-
-  //   if (existingEntries.includes(baseGameLink))
-  //     console.log("base game link found, must create a symlink");
-  // });
+  const symlink = generateSymlink(gameCode);
 
   fs.appendFile(`${path}/links.txt`, gameLink, (err) => {
     if (err) throw err;
   });
 };
+
+// if (fs.existsSync(`${path}/links.txt`)) {
+//   fs.readFile(`${path}/links.txt`, "utf8", (err, data) => {
+//     let existingLinks = data.trim().split("\n");
+
+//     // TODO: check if the gameLink already exists
+//     if (existingLinks.some((link) => link.includes(gameCode))) {
+//       console.log("LAKSD already exists");
+//     }
+//   });
+
+//   return;
+// }
 
 module.exports = createLink;
