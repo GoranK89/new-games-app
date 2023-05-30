ea4870e0bb03aa5f154d85d63bc6d3c851353f1e
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-05-30 22:48:14 +0200

    MGSD & MGSM game codes folder creation logic

--- createFolders/createGameFolders.js
+++ createFolders/createGameFolders.js
@@ -2,19 +2,45 @@ const fs = require('fs');
 const createLaunchFolder = require('./createLaunchFolder');
 const createGameDescription = require('../createFiles/createGameDescription');
 
+const addGameCodesToSet = (gameCode) => {
+  if (gameCode.includes('MGSD') || gameCode.includes('MGSM')) {
+    const MGScode = processMGScodes(gameCode);
+    return MGScode;
+  } else {
+    const defaultGameCode = processDefaultCodes(gameCode);
+    return defaultGameCode;
+  }
+};
+
+const processDefaultCodes = (gameCode) => {
+  const gameCodeParts = gameCode.split('_');
+  const lastPart = Number(gameCodeParts[gameCodeParts.length - 1]);
+  if (!isNaN(lastPart)) {
+    gameCodeParts.pop();
+  }
+  const gameName = gameCodeParts.join('_');
+  return gameName;
+};
+
+const processMGScodes = (gameCode) => {
+  const gameCodeParts = gameCode.split('_');
+  const lastPart = gameCodeParts[gameCodeParts.length - 1];
+  const last2Letters = lastPart.slice(-2);
+  const last2Numbers = Number(last2Letters);
+  gameCodeParts.shift();
+  if (!isNaN(last2Numbers)) {
+    gameCodeParts.pop();
+  }
+  const noGpGameCode = gameCodeParts.join('_');
+  return noGpGameCode;
+};
+
 const createGameFolders = (path, gameCodes) => {
   const gameNames = new Set();
 
   gameCodes?.forEach((gameCode) => {
     const folderPath = `${path}/${gameCode}`;
-    const gameCodeParts = gameCode.split('_');
-    const lastPart = Number(gameCodeParts[gameCodeParts.length - 1]);
-
-    if (!isNaN(lastPart)) {
-      gameCodeParts.pop();
-    }
-
-    const gameName = gameCodeParts.join('_');
+    const gameName = addGameCodesToSet(gameCode);
 
     if (!gameNames.has(gameName)) {
       gameNames.add(gameName);
